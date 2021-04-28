f479d81a85e6cb415af9ffbfc9b026e90558b0ff
boomermath <63861512+boomermath@users.noreply.github.com>
2021-04-28 13:06:11 -0400

    Create 1DataTypes.js

---
+++ 1DataTypes.js
@@ -0,0 +1,25 @@
+//FIRST LESSON
+
+Javascript data types:
+
+Undefined: undefined
+Boolean: true or false
+Number: 1,2,3,4, //etc, negative numbers too
+String: "this is a string. Strings hold text between '' or double quotes"
+BigInt: Like number, but anything bigger than 2^32
+
+
+//other kinda confusing stuff
+null //sort of like 0, but not really, null is a, well, null value, it means zilch, nada, but its not equal to 0
+
+//Other ones include objects and functions, but we can circle back later
+
+antoher useful thing
+
+use console.log, to log a value to the console (where you can see output)
+
+console.log(3) //outputs three
+
+in order to find out the type of a value, use typeof
+
+console.log(typeof "string") //outputs "string"
