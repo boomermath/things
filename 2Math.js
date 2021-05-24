9cb455d83fa752ba1bcdf6f09580edea2c49d0cd
boomermath <63861512+boomermath@users.noreply.github.com>
2021-05-24 19:25:46 -0400

    Update 2Math.js

--- 2Math.js
+++ 2Math.js
@@ -7,3 +7,24 @@ Mathematical operators
 ** //exponents
 
 You can also use parenthesis () like in regular order of operations
+
+Clarification about math 
+
+//when using let
+
+let i = 0;
+
+i /* i is being reassigned */ = i /* this is the current value of i */ + 1;
+//same as
+i += 1
+
+i = i * 2
+//same as
+i *= 2;
+
+//So you can do more advanced operations like
+
+i = Math.sqrt(i);
+
+//
+//essentially, you are taking the current value of i, doing math with it, and then making that value the value of i
